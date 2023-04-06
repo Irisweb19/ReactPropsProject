3bc675e048371dba7db8f68e76b6384626b3849c
MAC AIR <macair@MACs-MacBook-Air.local>
2023-04-06 15:11:08 +0100

    react Props

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Person from './components/profile/Person'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <Person fullName='Slyvanus' bio=' JSHDVXKNM,ASZDCX HJMASZXCKJNZXCM' profession='Software Engineer'/>
+    </div>
+  )
+}
+
+export default App
